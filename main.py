0b4263321980c0216d698c3e2042150e30183b91
Tim Weri <ng.duc.tahn@gmail.com>
2018-01-12 01:44:52 -0500

    Changed output files' names

--- main.py
+++ main.py
@@ -4,9 +4,9 @@ import urllib
 def main():
     menu_validator = MenuValidator()
     menu_validator.input_from_url("https://backend-challenge-summer-2018.herokuapp.com/challenges.json?id=1", 1)
-    output("output1.out", menu_validator.result())
+    output("main-challenge.out", menu_validator.result())
     menu_validator.input_from_url("https://backend-challenge-summer-2018.herokuapp.com/challenges.json?id=2", 1)
-    output("output2.out", menu_validator.result())
+    output("extra-challenge.out", menu_validator.result())
 
 def output(filename, data):
     with open(filename, 'w') as outputfile:
